e22ac80206c52f3d2d06568b2ba4a7cc6ca1ef75
Eduardo Branco <75887699+dEvBranco@users.noreply.github.com>
2021-03-22 15:30:46 +0000

    Navbar

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 
 import Product from './Product/Product';
+import useStyles from './styles';
 
 const products = [
     {id: 1, name: 'Shoes', description: 'Running shoes.', price: '€5', image:'https://cdn1.dynafit.com/media/image/f0/69/2c/Dynafit_S20_Running-Shoes_Women_800x800.jpg' },
@@ -9,8 +10,11 @@ const products = [
 ];
 
 const Products = () => {
+    const classes = useStyles();
+
     return(
-        <main>
+        <main className={classes.content}>
+            <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 {products.map((product)=>(
                     <Grid item key={product.id} xs={12} sm={6} md={4} lg={3}>
