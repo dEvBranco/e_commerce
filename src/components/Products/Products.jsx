8c6fc0f52c6fbb336b89492ae86c554605d7a65e
Eduardo Branco <75887699+dEvBranco@users.noreply.github.com>
2021-03-22 17:45:14 +0000

    Adding products

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,12 +4,8 @@ import { Grid } from '@material-ui/core';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const products = [
-    {id: 1, name: 'Shoes', description: 'Running shoes.', price: '€5', image:'https://cdn1.dynafit.com/media/image/f0/69/2c/Dynafit_S20_Running-Shoes_Women_800x800.jpg' },
-    {id: 2, name: 'Macbook', description: 'Apple macbook.', price: '€10', image:'https://www.discoazul.pt/uploads/media/images/apple-macbook-pro-8gb-256gb-gris-espacial-myd82y-a-12.jpg' },
-];
 
-const Products = () => {
+const Products = ({ products }) => {
     const classes = useStyles();
 
     return(
