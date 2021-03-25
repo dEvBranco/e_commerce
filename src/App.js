3865fd7fef656250f6e0dc4bb5d4b97927a48bb8
Eduardo Branco <75887699+dEvBranco@users.noreply.github.com>
2021-03-25 15:30:07 +0000

    Final update

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 const App = () => {
     const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState([]);
+    const [cart, setCart] = useState({});
+    const [order, setOrder] = useState({});
+    const [errorMessage, setErrorMessage] = useState('');
 
     const fetchProducts = async () => {
         const { data } = await commerce.products.list();
@@ -41,6 +43,23 @@ const App = () => {
         setCart(cart);
     }
 
+    const refreshCart = async () => {
+        const newCart = await commerce.cart.refresh();
+
+        setCart(newCart);
+    }
+
+    const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+        try {
+            const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
+
+            setOrder(incomingOrder);
+            refreshCart();
+        } catch (error) {
+            setErrorMessage(error.data.error.message);
+        }
+    }
+
     useEffect(() => {
         fetchProducts();
         fetchCart();
@@ -52,7 +71,7 @@ const App = () => {
         <Router>
             <div>
                 <NavBar totalItems={cart.total_items} />
-                <switch>
+                <Switch>
                     <Route exact path="/">
                         <Products products={products} onAddToCart={handleAddToCart} />
                     </Route>
@@ -65,9 +84,14 @@ const App = () => {
                         />
                     </Route>
                     <Route exact path="/checkout">
-                        <Checkout cart={cart} />
+                        <Checkout 
+                            cart={cart}
+                            order={order}
+                            onCaptureCheckout={handleCaptureCheckout}
+                            error={errorMessage}
+                        />
                     </Route>
-                </switch>
+                </Switch>
             </div>
         </Router>
     );
